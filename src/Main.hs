d94b9bc7768cc8b950a727c56034f556f4f69fc7
Vladimir Tikhonov <vladimir@tikhonov.by>
2015-09-28 13:14:53 +0300

    Initial

---
+++ src/Main.hs
@@ -0,0 +1,50 @@
+module Main where
+
+import           Options.Applicative
+
+data Opts = Opts
+  { input     :: String
+  , output    :: String
+  , clusters  :: Int
+  , metric    :: String
+  , precision :: Double
+  } deriving(Show)
+
+spec :: Parser Opts
+spec = Opts
+     <$> argument str
+         (  metavar "FILE"
+         <> help "Файл с данными для кластеризации" )
+     <*> option str
+         (  long "output"
+         <> short 'o'
+         <> metavar "FILE"
+         <> help "Файл, куда будут записаны результаты" )
+     <*> option auto
+         (  long "clusters"
+         <> short 'c'
+         <> metavar "COUNT"
+         <> help "Количество кластеров"
+         <> value 5 )
+     <*> option str
+         (  long "metric"
+         <> short 'm'
+         <> metavar "NAME"
+         <> help "Используемая метрика" )
+     <*> option auto
+         (  long "precision"
+         <> short 'p'
+         <> metavar "VALUE"
+         <> help "Точность вычислений"
+         <> value 0.01 )
+
+showOpts :: Opts -> IO ()
+showOpts opts = putStrLn $ "Аргументы: " ++ show opts
+
+main :: IO ()
+main = execParser opts >>= showOpts
+ where
+   opts = info (helper <*> spec)
+     ( fullDesc
+    <> progDesc "Кластеризует переданные данные методом FCM"
+    <> header "fcm - утилита для кластеризации данных" )
